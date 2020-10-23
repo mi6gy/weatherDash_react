fccd22fa61c8b28397f82443558d084a4c854315
Miguel Jimenez <mjimenez08@gmail.com>
2020-10-22 23:11:28 -0400

    added search bar, location, background images, CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const api = {
+  key: "f27a583255ce319d707475582d54fe53",
+  base: "https//api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
+        </div>
+        <div className="location-box">
+          <div className="location">Newark NJ USA</div>
+<div className="date"></div>
+        </div>
+      </main>
     </div>
   );
 }
